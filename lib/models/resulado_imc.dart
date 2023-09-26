e4e6fdcc1eb56d384934d9a4e1921deb8c8f4eb5
Mestre Dos Lances <appmestredoslances@gmail.com>
2023-09-26 10:20:34 -0300

    add models

---
+++ lib/models/resulado_imc.dart
@@ -0,0 +1,9 @@
+import 'package:app_calculadora_imc_view/models/pessoa.dart';
+
+class ResuladoImc {
+  Pessoa pessoa;
+  double imc;
+  String resultado;
+
+  ResuladoImc(this.pessoa,this.imc,this.resultado);
+}
